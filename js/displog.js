029e36d3dfe6a12a3a3da2441a35aae6c447c538
hanndoddi <hanndoddi@gmail.com>
2026-03-17 19:32:20 +0000

    log timestamp fix

--- js/displog.js
+++ js/displog.js
@@ -2,7 +2,7 @@ function renderLogTable(log) {
   let html = "<table border='1'><tr><th>Tími</th><th>Afl</th><th>Orka</th></tr>";
   log.forEach(entry => {
     html += `<tr>
-      <td>${entry.timestamp}</td>
+      <td>${new Date(entry.timestamp).toLocaleString('is-IS')}</td>
       <td>${entry.power}</td>
       <td>${entry.energy}</td>
     </tr>`;
